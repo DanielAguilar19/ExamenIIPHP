a5a9f76e2ac872209e42fd5a9e9563992138a5db
Daniel Aguilar <aguilardaniel631@gmail.com>
2024-08-22 21:52:22 -0600

    Cambios finales

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
+//Obtener todos por tabla
 Route::get('/productos', [ProductoController::class, 'getAllProductos'])->name('getProductos');
 Route::get('/empleados', [EmpleadoController::class, 'getAllEmpleados'])->name('getEmpleados');
 Route::get('/proveedor', [ProveedoresController::class, 'getAllProveedores'])->name('getProveedores');
-
-Route::get('/form/prod', [ProductoController::class, 'getFormProductos'])->name('formProductos');
-Route::get('/form/emp', [EmpleadoController::class, 'getFormEmpleados'])->name('formEmpleados');
-Route::get('/form/prov', [ProveedoresController::class, 'getFormProveedores'])->name('formProveedores');
-
+//formularios de todas las tablas
+Route::get('/form/prod', [ProductoController::class, 'getFormProductos'])->name('formuProdu');
+Route::get('/form/emp', [EmpleadoController::class, 'getFormEmpleados'])->name('formuEmple');
+Route::get('/form/prov', [ProveedoresController::class, 'getFormProveedores'])->name('formuProve');
+//ruta que hace post y guarda el nuevo inserto en pase de datos
 Route::post('/producto/crear', [ProductoController::class, 'postFormProductos'])->name('crearProd');
 Route::post('/empleado/crear', [EmpleadoController::class, 'postFormEmpleados'])->name('crearEmp');
 Route::post('/proveedor/crear', [ProveedoresController::class, 'postFormProveedores'])->name('crearProve');
